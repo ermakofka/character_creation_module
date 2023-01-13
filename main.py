e150337b10b555461947ba6e0269a2e16fb19a0e
Danil Ermakov <danilwe2000@gmail.com>
2023-01-14 00:50:24 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Показывает нанесенный урон."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный:'
                 '{5 + randint(3, 5)}')
@@ -14,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Показывает сколько урона заблокировано."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +25,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Применение спец возможностей."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение'
                 '«Выносливость {80 + 25}»')
@@ -35,6 +38,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Характеристики выбранного класса."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -59,6 +63,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Описание классов."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -81,6 +86,7 @@ def choice_char_class() -> str:
 
 
 def main():
+    """Указание имени и класса."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
